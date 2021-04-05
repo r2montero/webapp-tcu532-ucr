6141ae5f0d489037eb460bdec529f86dd9936a22
r2montero <jose.rojasmontero@ucrso.info>
2021-04-04 19:44:56 -0600

    Categories CRUD complete

--- webapp-tcu532-ucr/src/routes/api/categories.js
+++ webapp-tcu532-ucr/src/routes/api/categories.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
         res.json(categories);
     }
 
-});
+})
 
 // @route GET /api/categories/:id
 // @description Get single category by id
@@ -41,22 +41,94 @@ router.get('/:id', async (req, res) => {
         });
     };
 
-});
+})
 
 // @route POST /api/categories
 // @description add new category
 // @access Restricted
-router.post('/', (req, res) => {
-    Category.create(req.body)
-        .then(() => res.json({
-            msg: 'Categoria agregada correctamente'
-        }))
-        .catch(err => console.log(err),
-            res.status(400)
-            .json({
-                error: 'No fue posible agregar la categoria'
-            }));
-});
+router.post('/', async (req, res) => {
+    const { name, description } = req.body;
+    await Category.findOne({ name }, async (err, category) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ msg: 'No fue posible agregar la categoria' });
+        };
+        if (category) {
+            res.status(409).json({ msg: 'Categoria ya existe' });
+        } else {
+            const category = new Category(
+                {
+                    name,
+                    description
+                }
+            );
+            category.save((err, category) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json({ msg: 'No fue posible agregar la categoria' });
+                };
+                if (category) {
+                    res.status(201).json({ msg: 'Categoria agregada correctamente' });
+                };
+            });
+        };
+    });
+})
 
+// @route GET api/categories/:id
+// @description Update a category
+// @access Restricted
+router.put('/:id', async (req, res) => {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+        const { name, description } = req.body;
+        const category = await Category.findById(req.params.id);
+        if (category === null) {
+            res.status(404).json({
+                msg: 'No hay ninguna categoría con la identificacion proporcionada'
+            });
+        } else {
+            const exists = await Category.findOne({ name });
+            if (exists) {
+                console.log(exists);
+                res.status(409).json({ msg: 'Categoria ya existe' });
+            } else {
+                category.name = name;
+                category.description = description;
+                category.save((err, category) => {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).json({ msg: 'No fue posible agregar la categoria' });
+                    };
+                    if (category) {
+                        res.status(200).json({ msg: 'Categoria actualizada correctamente' });
+                    };
+                });
+            }
+        };
+    } else {
+        res.status(400).json({
+            msg: 'Formato de identificacion no valido'
+        });
+    };
+})
+
+router.delete('/:id', async (req, res) => {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+        Category.findByIdAndRemove(req.params.id, (err, category) => {
+            if (!category) {
+                res.status(404).json({ msg: 'No existe una categoria con la identificacion proporcionada' });
+            }
+            if (err) {
+                console.log(err);
+                res.status(500).json({ msg: 'No fue posible eliminar la categoria' });
+            };
+            res.status(200).json({ msg: 'Categoria eliminada correctamente' });
+        });
+    } else {
+        res.status(400).json({
+            msg: 'Formato de identificacion no valido'
+        });
+    }
+})
 
 module.exports = router;
